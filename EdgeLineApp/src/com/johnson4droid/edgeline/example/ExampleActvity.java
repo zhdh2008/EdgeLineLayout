da1936f30f6a09befff9579b097741cbfb6b5ce2
zhdh2008 <zhdh2008@gmail.com>
2015-04-08 20:38:14 +0800

    [modified] this is a test commit

---
+++ EdgeLineApp/src/com/johnson4droid/edgeline/example/ExampleActvity.java
@@ -0,0 +1,15 @@
+package com.johnson4droid.edgeline.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ExampleActvity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
